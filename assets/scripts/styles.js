2a17de411fcb04e0d3269864afe752680f3b0265
A K <imagineer023@me.com>
2015-08-02 00:11:21 -0400

    Global header: Redesigned global header

---
+++ assets/scripts/styles.js
@@ -0,0 +1,39 @@
+$(function() {
+
+  // Global header styles
+  $('#gh-search').on('focusin focusout', function() {
+    $('#gh-search-form').toggleClass('gh-search-form-focus');
+  });
+  $('#gh-spacer').css('margin-top', $('#globalheader').height());
+
+  // Nav Drawer
+  var $navDrawerWrapper = $('#nav-drawer-wrapper');
+  var navDrawerTransition = TweenMax.from($navDrawerWrapper, 0.2,
+    {
+      height: 0,
+      transformOrigin: "left top",
+      paused: true,
+      ease: Back.easeOut.config(1.5),
+      onReverseComplete: function() {
+        $navDrawerWrapper.addClass('nav-drawer-closed')
+      }
+    });
+  $('#nav-drawer-toggle').click(function() {
+    $(this).toggleClass('nav-drawer-toggle-active');
+    if ($navDrawerWrapper.hasClass('nav-drawer-closed')) {
+      $navDrawerWrapper.removeClass('nav-drawer-closed');
+      navDrawerTransition.restart();
+    } else {
+      navDrawerTransition.reverse();
+    }
+  });
+
+  // DTP buttons
+  var $dtpButtons = $('.dtp-button');
+  $dtpButtons.on('mouseup mouseleave', function() {
+    $(this).removeClass('dtp-button-active');
+  });
+  $dtpButtons.on('mousedown', function() {
+    $(this).addClass('dtp-button-active');
+  });
+});
